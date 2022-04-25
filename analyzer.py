7cbf74f44364ae963ff599d13addfe570db7fada
Landon Palmer <landonjpalmer@gmail.com>
2022-04-25 13:00:13 -0500

    polishing functionality and docstring

--- analyzer.py
+++ analyzer.py
@@ -4,6 +4,27 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 
+
+''' DOCSTRING:
+
+DESCRIPTION:
+    Welcome to this prototype for analyzing website accessibility!
+    This script will use webscraping to analyze the DOM of a given site.
+    It will consider the following criteria:
+        1. Whether or not the <html> tag includes the 'lan' attribute (for translating screen readers)
+        2. Whether or not headings [any <h1>...<h6>] are included in content (for screen reader organization)
+        3. Whether or not all included images have alt text (also screen readers/visually impaired)
+    Enter a URL to see the results of this analysis! See usage below.
+
+USAGE:
+    `python analyzer.py 0` for regular usage, prompted for URL (including surrounding quotes [sorry, lazy])
+    `python analyzer.py 1` for ~debug~ that polls my personal accessible(ish) website
+    OUTPUT is printed object in form of `('HAS HTML LAN: {T/F}', 'HAS HEADINGS: {T/F}', 'HAS ALT TEXT: {T/F}')`
+
+Enjoy :D
+'''
+
+
 def get_soup_from_website(url):
     print("checking out " + url)
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
@@ -12,8 +33,9 @@ def get_soup_from_website(url):
     return soup
 
 def html_tag_includes_lan(soup, debug=False):
-    html_tag = soup.html["lang"]
+    html_tag = soup.html
     if(debug): print("html tag: "); print(html_tag)
+    return html_tag.has_attr("lang")
 
 def includes_headings(soup, debug=False):
     headings = soup.find_all(re.compile("^h[1-6]$"))
@@ -22,12 +44,13 @@ def includes_headings(soup, debug=False):
 
 def images_include_alt_text(soup, debug=False):
     alt_images = soup.find_all("img", alt=False)
-    if debug: print("non-alt images: "); print(alt_images)
-
-
-
+    if debug: print("non-alt images: "); print(alt_images); print(type(alt_images))
+    for img in alt_images: # quick workaround for bs4.resultset
+        return False
+    return True
 
 # ===============================================================
+
 debug = False
 if(len(sys.argv) > 1 and sys.argv[1] == "1"): debug = True
 
@@ -37,21 +60,36 @@ if debug:
     soup = get_soup_from_website(debug_website)
 
     # call methods
-    html_tag_includes_lan(soup, 1)
+    print(html_tag_includes_lan(soup, 1))
     includes_headings(soup, 1)
     images_include_alt_text(soup, 1)
     exit(0)
 
+else:
+    class AccessibilityReport:
+
+        def __init__(self, u = "NO URL", l=False, h=False, a=False):
+            self.url = u
+            self.html_includes_lan_attribute = l
+            self.content_includes_headings = h
+            self.images_include_alt_text = a
+
+        def get_data(self):
+            print("RESULTS FOR: " + self.url)
+            print(
+                "HAS HTML LAN: " + str(self.html_includes_lan_attribute), 
+                "HAS HEADINGS: " + str(self.content_includes_headings), 
+                "HAS ALT TEXT: " + str(self.images_include_alt_text)
+            )
 
 
-URL = input("Enter a website here: ")
+    url = input("Enter a website in quotes: ")
+    soup = get_soup_from_website(url)
+    lan = html_tag_includes_lan(soup)
+    headings = includes_headings(soup)
+    alt = images_include_alt_text(soup)
+    result = AccessibilityReport(url, lan, headings, alt)
 
-accessible_score = 0
-accessible_max = 10
+    result.get_data()
 
 
-if(includes_headings(soup)):
-    accessible_score += 1
-
-
-print("accessible score: ", accessible_score)
